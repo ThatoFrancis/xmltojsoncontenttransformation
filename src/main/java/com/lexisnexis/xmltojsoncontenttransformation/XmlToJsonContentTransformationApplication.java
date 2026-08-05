72bca09482acaa265ddf4c38177835f581935cc6
ThatoFrancis <thatofrancis@gmail.com>
2026-08-05 14:35:15 +0200

    initial push

---
+++ src/main/java/com/lexisnexis/xmltojsoncontenttransformation/XmlToJsonContentTransformationApplication.java
@@ -0,0 +1,12 @@
+package com.lexisnexis.xmltojsoncontenttransformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlToJsonContentTransformationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlToJsonContentTransformationApplication.class, args);
+    }
+}
